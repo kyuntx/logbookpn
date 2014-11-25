98dc189e78de4a0e4b58f3d8cd39d557e054121e
kyun <kyun@clovertown.jp>
2014-11-26 03:00:27 +0900

    Push通知機能に遠征・入渠ごとのOn/Off, Priority設定を追加

--- main/logbook/gui/background/AsyncExecApplicationMain.java
+++ main/logbook/gui/background/AsyncExecApplicationMain.java
@@ -198,15 +198,16 @@ public final class AsyncExecApplicationMain extends Thread {
         public void run() {
             // 現在時刻
             Date now = Calendar.getInstance().getTime();
-            List<String> notice = new ArrayList<String>();
+            List<String> noticeMission = new ArrayList<String>();
+            List<String> noticeNdock = new ArrayList<String>();
             boolean visibleHome = false;
             // 遠征を更新する
-            if (this.updateDeck(now, notice)) {
+            if (this.updateDeck(now, noticeMission)) {
                 Sound.randomExpeditionSoundPlay();
                 visibleHome |= AppConfig.get().isVisibleOnReturnMission();
             }
             // 入渠を更新する
-            if (this.updateNdock(now, notice)) {
+            if (this.updateNdock(now, noticeNdock)) {
                 Sound.randomDockSoundPlay();
                 visibleHome |= AppConfig.get().isVisibleOnReturnBathwater();
             }
@@ -217,6 +218,9 @@ public final class AsyncExecApplicationMain extends Thread {
                 // バルーンツールチップを表示する
                 try {
                     // 遠征・入渠のお知らせ
+                    List<String> notice = new ArrayList<String>();
+                    notice.addAll(noticeMission);
+                    notice.addAll(noticeNdock);
                     if (notice.size() > 0) {
                         ToolTip tip = new ToolTip(this.main.getShell(), SWT.BALLOON
                                 | SWT.ICON_INFORMATION);
@@ -229,12 +233,17 @@ public final class AsyncExecApplicationMain extends Thread {
                     LOG.warn("お知らせの表示に失敗しました", e);
                 }
             }
-            if (AppConfig.get().getNotifyProwl() || AppConfig.get().getNotifyNMA()
-                    || AppConfig.get().getNotifyImKayac()) {
-                // Push 通知
-                if (notice.size() > 0) {
-                    PushNotify.add(StringUtils.join(notice, "\r\n"));
-                }
+
+            // Push通知 遠征
+            if ((noticeMission.size() > 0) && AppConfig.get().getPushMission()) {
+                PushNotify.add(StringUtils.join(noticeMission, "\r\n"), "遠征",
+                        AppConfig.get().getPushPriorityMission());
+            }
+
+            // Push通知 入渠
+            if ((noticeNdock.size() > 0) && AppConfig.get().getPushNdock()) {
+                PushNotify.add(StringUtils.join(noticeNdock, "\r\n"), "入渠",
+                        AppConfig.get().getPushPriorityNdock());
             }
         }
 
